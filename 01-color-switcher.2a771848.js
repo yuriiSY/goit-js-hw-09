e03550d836164fa8233495b7ed4a28dd129a446a
yuriiSY <108358132+yuriiSY@users.noreply.github.com>
2023-11-15 21:09:46 +0000

    Deploying to gh-pages from @ yuriiSY/goit-js-hw-09@42249e14b435f857576b4cdc0fd7fcf5d6a9539f 🚀

---
+++ 01-color-switcher.2a771848.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n;t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.2a771848.js.map
